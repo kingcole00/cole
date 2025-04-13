016c665888f44f4ea64f9be9a22c42e79c9e4331
kingcole00 <kingcole001@yahoo.com>
2025-04-13 17:36:23 -0400

    Update TweetEmbed.jsx

--- src/components/TweetEmbed.jsx
+++ src/components/TweetEmbed.jsx
@@ -4,18 +4,21 @@ export default function TweetEmbed() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
-    script.setAttribute('async', '');
-    containerRef.current.appendChild(script);
+    // Check if the script has already been loaded
+    if (!document.querySelector('script[src="https://platform.twitter.com/widgets.js"]')) {
+      const script = document.createElement('script');
+      script.src = 'https://platform.twitter.com/widgets.js';
+      script.async = true;
+      document.body.appendChild(script);
+    } else if (window.twttr?.widgets) {
+      window.twttr.widgets.load(containerRef.current);
+    }
   }, []);
 
   return (
     <div ref={containerRef} className="flex justify-center mt-8">
       <blockquote className="twitter-tweet" data-theme="dark">
-        <a href="https://x.com/RobinHooding/status/1745131794227435724">
-          https://x.com/RobinHooding/status/1745131794227435724
-        </a>
+        <a href="https://x.com/RobinHooding/status/1745131794227435724">Loading tweet…</a>
       </blockquote>
     </div>
   );
