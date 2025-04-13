f773af5115d2526833118d2b6e9501832d17cb09
kingcole00 <kingcole001@yahoo.com>
2025-04-13 17:43:46 -0400

    Update TweetEmbed.jsx

--- src/components/TweetEmbed.jsx
+++ src/components/TweetEmbed.jsx
@@ -4,27 +4,29 @@ export default function TweetEmbed() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    // Load the Twitter script if it hasn’t already been loaded
-    if (!window.twttr) {
-      const script = document.createElement("script");
-      script.src = "https://platform.twitter.com/widgets.js";
-      script.async = true;
-      script.onload = () => {
-        if (window.twttr && window.twttr.widgets) {
-          window.twttr.widgets.load(containerRef.current);
-        }
-      };
-      document.body.appendChild(script);
-    } else {
-      // Twitter script already loaded, trigger widget rendering
-      window.twttr.widgets.load(containerRef.current);
-    }
+    // Inject Twitter widget script
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    script.setAttribute('async', '');
+    script.setAttribute('charset', 'utf-8');
+    containerRef.current.appendChild(script);
   }, []);
 
   return (
     <div ref={containerRef} className="flex justify-center mt-8">
-      <blockquote className="twitter-tweet" data-theme="dark">
-        <a href="https://x.com/RobinHooding/status/1745131794227435724">Loading tweet…</a>
+      <blockquote className="twitter-tweet">
+        <p lang="en" dir="ltr">
+          I will be hosting + doing some comedy in between sets at this show on the 23rd <br />
+          Help a young mom fight cancer ✨🙏<br />
+          Rock for Hope: A Benefit for Noelle at 565 LIVE<br />
+          Noelle DeFazio, a mother of two from Pittsburgh, PA, is battling stage 4 cervical cancer. She has undergone multiple…{' '}
+          <a href="https://t.co/OzrHrC1xOa">pic.twitter.com/OzrHrC1xOa</a>
+        </p>
+        — King Cole (
+        <a href="https://twitter.com/RobinHooding/status/1910142967915913700?ref_src=twsrc%5Etfw">
+          @RobinHooding
+        </a>){' '}
+        <a href="https://twitter.com/RobinHooding/status/1910142967915913700?ref_src=twsrc%5Etfw">April 10, 2025</a>
       </blockquote>
     </div>
   );
