5198da22bc18d1fe272e4b7b2434115b596e25d6
kingcole00 <kingcole001@yahoo.com>
2025-04-13 17:26:42 -0400

    Update TweetEmbed.jsx

--- src/components/TweetEmbed.jsx
+++ src/components/TweetEmbed.jsx
@@ -1,1 +1,22 @@
 
+import { useEffect } from 'react';
+
+export default function TweetEmbed() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div className="flex justify-center mt-8">
+      <blockquote
+        className="twitter-tweet"
+        data-theme="dark"
+      >
+        <a href="https://x.com/RobinHooding/status/1234567890123456789"></a>
+      </blockquote>
+    </div>
+  );
+}
